763aa7db43785ce78cdff89a9eddc5f5e22db7e3
pradhuman2022 <42096528+pradhuman2022@users.noreply.github.com>
2019-05-16 11:45:46 +0530

    Create Thread2.java
    Thread created by implementing Runnable Interface !

---
+++ Multithreading/Thread2.java
@@ -0,0 +1,24 @@
+public class Thread2
+{
+ public static void main(String ar[])
+  {
+    MyRunnable mr = new MyRunnable() ;
+    Thread t = new Thread(mr);
+    t.start() ;
+    for(int i = 0 ; i < 10 ; i++)
+    {
+     System.out.println("Main Thread") ;
+    }
+  }
+}
+
+class MyRunnable implements Runnable
+{
+ public void run()
+ {
+   for(int i = 0 ; i < 10 ; i++)
+    {
+     System.out.println("Child Thread");
+    }
+ }
+}
